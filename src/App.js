1652ac59ee0f37422e3b9cd78e5fbc3b2c53f0da
Yvonne Moser <yvonne_moser@outlook.de>
2019-05-29 16:29:01 +0200

    button changed chat

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Profile from "./pages/Profile"
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
